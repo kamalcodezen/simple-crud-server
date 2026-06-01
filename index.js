f41bf2c8f402372d280ff72ea4631fc94cc56e8f
kamalcodezen <kamaluddin7908@gmail.com>
2026-06-01 15:09:26 +0530

    delete server added

--- index.js
+++ index.js
@@ -49,7 +49,15 @@ const run = async () => {
         })
 
 
+        app.delete("/users/:id", async (req, res) => {
+            const id = req.params.id
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
 
+        })
 
 
 
